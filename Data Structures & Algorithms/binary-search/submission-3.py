3c3c43d76ef3f077b48b131b5cc2276a1038fdc7
Sanchya Bhagat <sanchya.bhagat@gmail.com>
2026-05-13 16:36:22 -0700

    Add: binary-search - submission-3

---
+++ Data Structures & Algorithms/binary-search/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums)-1 # initialize at edges
+        while l <= r:
+            # prevent potential overflow in java or other bound languages. Not an issue in python
+            m = (r + l) // 2 
+
+            if nums[m] == target:
+                return m
+            
+            elif nums[m] > target:
+                r = m-1
+            
+            elif nums[m] < target:
+                l = m+1
+        
+        return -1
+
+        
