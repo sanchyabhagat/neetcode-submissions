27b6384f5dbd3fa2962ee431d4f9cc99909b0849
Sanchya Bhagat <sanchya.bhagat@gmail.com>
2026-08-03 15:49:03 -0700

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # cannot rob two consecutive houses
+        rob1, rob2 = 0, 0
+
+        for n in nums:
+            temp = max(rob2, rob1 + n)
+            rob1 = rob2
+            rob2 = temp
+        
+        return rob2
+
+        
